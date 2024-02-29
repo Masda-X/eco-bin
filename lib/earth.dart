a8d5986cdc1a5a9b3d206bcc7c2aa7dcbd65fa81
Masda <matlabnewone@gmail.com>
2024-02-29 13:55:55 +0400

    Remove unused import statement

--- lib/earth.dart
+++ lib/earth.dart
@@ -8,7 +8,6 @@ import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class Earth extends PositionComponent
     with

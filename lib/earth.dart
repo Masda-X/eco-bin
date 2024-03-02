61e91fb364789fd95ce9bc5aefe995ea283679ef
Masda <matlabnewone@gmail.com>
2024-03-02 07:59:48 +0400

    Remove unused imports and commented code in earth.dart

--- lib/earth.dart
+++ lib/earth.dart
@@ -1,10 +1,9 @@
 import 'package:earth/config.dart';
 import 'package:earth/game.dart';
-import 'package:flame_noise/flame_noise.dart';
 
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
-import 'package:flame/effects.dart';
+
 import 'package:flame/events.dart';
 
 import 'package:flutter/foundation.dart';
@@ -85,28 +84,28 @@ class Earth extends CircleComponent
   void onDoubleTapDown(DoubleTapDownEvent event) {
     // add(RemoveEffect(delay: 1));
 
-    add(
-      SequenceEffect(
-        [
-          MoveEffect.by(
-            Vector2(50, 0),
-            NoiseEffectController(
-              duration: 1,
-              noise: PerlinNoise(frequency: 20),
-            ),
-          ),
-          // MoveEffect.by(Vector2.zero(), LinearEffectController(2)),
-          // MoveEffect.by(
-          //   Vector2(0, 10),
-          //   NoiseEffectController(
-          //     duration: 1,
-          //     noise: PerlinNoise(frequency: 10),
-          //   ),
-          // ),
-        ],
-        infinite: false,
-      ),
-    );
+    // add(
+    //   SequenceEffect(
+    //     [
+    //       MoveEffect.by(
+    //         Vector2(50, 0),
+    //         NoiseEffectController(
+    //           duration: 1,
+    //           noise: PerlinNoise(frequency: 20),
+    //         ),
+    //       ),
+    //       MoveEffect.by(Vector2.zero(), LinearEffectController(2)),
+    //       MoveEffect.by(
+    //         Vector2(0, 10),
+    //         NoiseEffectController(
+    //           duration: 1,
+    //           noise: PerlinNoise(frequency: 10),
+    //         ),
+    //       ),
+    //     ],
+    //     infinite: false,
+    //   ),
+    // );
   }
 
   @override

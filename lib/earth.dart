73e3accc10e281925ed3d3803f1ae2e4abd53d0a
Masda <matlabnewone@gmail.com>
2024-03-05 22:48:55 +0400

    Commented out onDragUpdate method in Earth class

--- lib/earth.dart
+++ lib/earth.dart
@@ -49,17 +49,17 @@ class Earth extends CircleComponent
     // );
   }
 
-  @override
-  void onDragUpdate(DragUpdateEvent event) {
-    position += event.localDelta;
+  // @override
+  // void onDragUpdate(DragUpdateEvent event) {
+  //   position += event.localDelta;
 
-    const minX = 50; // 10% margin on the left
-    const maxX = 1860; // 10% margin on the right
-    const minY = 50; // 10% margin on the top
-    const maxY = 1020; // 10% margin on the bottom
-    position.x = position.x.clamp(minX.toDouble(), maxX.toDouble());
-    position.y = position.y.clamp(minY.toDouble(), maxY.toDouble());
-  }
+  //   const minX = 50; // 10% margin on the left
+  //   const maxX = 1860; // 10% margin on the right
+  //   const minY = 50; // 10% margin on the top
+  //   const maxY = 1020; // 10% margin on the bottom
+  //   position.x = position.x.clamp(minX.toDouble(), maxX.toDouble());
+  //   position.y = position.y.clamp(minY.toDouble(), maxY.toDouble());
+  // }
 
   // ignore: prefer_const_constructors
 

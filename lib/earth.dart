4d68c3ca27c52c5e9703c240e7adeaa966301383
Masda <matlabnewone@gmail.com>
2024-03-08 22:24:02 +0400

    Add imports for Banana and Plastic classes

--- lib/earth.dart
+++ lib/earth.dart
@@ -1,4 +1,6 @@
+import 'package:earth/banana.dart';
 import 'package:earth/game.dart';
+import 'package:earth/plastic.dart';
 
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
@@ -10,6 +12,7 @@ import 'package:flutter/material.dart';
 
 class Earth extends CircleComponent
     with
+        CollisionCallbacks,
         HoverCallbacks,
         DragCallbacks,
         TapCallbacks,
@@ -25,9 +28,6 @@ class Earth extends CircleComponent
           paint: Paint()..color = Color.fromARGB(0, 244, 67, 54),
         );
 
-  // final Paint paint;
-  // final Vector2 position;
-
   @override
   Future<void> onLoad() async {
     super.onLoad();
@@ -40,56 +40,7 @@ class Earth extends CircleComponent
       position: Vector2(-10, -2),
       // anchor: Anchor.topCenter, // BU ONEMLIDI  DO NOT ADD ANCHOR HERE
     ));
-    // game.world.add(this);
-    // add(
-    //   MoveEffect.by(
-    //     Vector2(0, 20),
-    //     InfiniteEffectController(SineEffectController(period: 1)),
-    //   ),
-    // );
   }
-
-  // @override
-  // void onDragUpdate(DragUpdateEvent event) {
-  //   position += event.localDelta;
-
-  //   const minX = 50; // 10% margin on the left
-  //   const maxX = 1860; // 10% margin on the right
-  //   const minY = 50; // 10% margin on the top
-  //   const maxY = 1020; // 10% margin on the bottom
-  //   position.x = position.x.clamp(minX.toDouble(), maxX.toDouble());
-  //   position.y = position.y.clamp(minY.toDouble(), maxY.toDouble());
-  // }
-
-  // FlameAudio.play('crash.wav');
-
-  // void onCollisionStart(
-  //     Set<Vector2> intersectionPoints, PositionComponent other) {
-  //   if (other is Plastic || other is Banana) {
-  //     add(
-  //       SequenceEffect(
-  //         [
-  //           MoveEffect.by(
-  //             Vector2(10, 0),
-  //             NoiseEffectController(
-  //               duration: 1,
-  //               noise: PerlinNoise(frequency: 20),
-  //             ),
-  //           ),
-  //           // MoveEffect.by(Vector2.zero(), LinearEffectController(2)),
-  //           // MoveEffect.by(
-  //           //   Vector2(0, 10),
-  //           //   NoiseEffectController(
-  //           //     duration: 1,
-  //           //     noise: PerlinNoise(frequency: 10),
-  //           //   ),
-  //           // ),
-  //         ],
-  //         infinite: false,
-  //       ),
-  //     );
-  //   }
-  // }
 
   @override
   void onDragEnd(DragEndEvent event) {
@@ -111,32 +62,7 @@ class Earth extends CircleComponent
   void onTapUp(event) {}
 
   @override
-  void onDoubleTapDown(DoubleTapDownEvent event) {
-    // add(RemoveEffect(delay: 1));
-
-    // add(
-    //   SequenceEffect(
-    //     [
-    //       MoveEffect.by(
-    //         Vector2(50, 0),
-    //         NoiseEffectController(
-    //           duration: 1,
-    //           noise: PerlinNoise(frequency: 20),
-    //         ),
-    //       ),
-    //       MoveEffect.by(Vector2.zero(), LinearEffectController(2)),
-    //       MoveEffect.by(
-    //         Vector2(0, 10),
-    //         NoiseEffectController(
-    //           duration: 1,
-    //           noise: PerlinNoise(frequency: 10),
-    //         ),
-    //       ),
-    //     ],
-    //     infinite: false,
-    //   ),
-    // );
-  }
+  void onDoubleTapDown(DoubleTapDownEvent event) {}
 
   @override
   void onTapCancel(event) {}
@@ -148,5 +74,24 @@ class Earth extends CircleComponent
   @override
   bool containsPoint(Vector2 point) {
     return position.distanceTo(point) <= size.x / 2;
+  }
+
+  int hitCount = 0;
+  @override
+  void onCollisionStart(
+      Set<Vector2> intersectionPoints, PositionComponent other) {
+    super.onCollisionStart(intersectionPoints, other);
+    if (other is Plastic || other is Banana) {
+      if (kDebugMode) {
+        print('objectHit');
+      }
+      hitCount++; // Increment the counter when Earth is hit by Plastic
+      if (hitCount >= 30) {
+        if (kDebugMode) {
+          print('objectHit 1');
+        }
+        removeFromParent();
+      }
+    }
   }
 }

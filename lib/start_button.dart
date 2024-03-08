f403060883ebaddbe44dd2aac83f2bdf14b3d9f7
Masda <matlabnewone@gmail.com>
2024-03-08 14:49:00 +0400

    Fix color transparency issue in Start button

--- lib/start_button.dart
+++ lib/start_button.dart
@@ -15,7 +15,7 @@ import 'package:flutter/material.dart';
 
 class Start extends RectangleComponent with TapCallbacks, HasGameRef<MyGame> {
   // ignore: prefer_const_constructors
-  Start() : super(paint: Paint()..color = Color.fromARGB(255, 33, 149, 243)) {
+  Start() : super(paint: Paint()..color = Color.fromARGB(0, 33, 149, 243)) {
     width = 640;
     height = 135;
     x = 100;
@@ -44,79 +44,81 @@ class Start extends RectangleComponent with TapCallbacks, HasGameRef<MyGame> {
   bool isCreatorRightAdded = false;
   @override
   void onTapDown(TapDownEvent event) {
-    if (!isPlayerAdded &&
-        !isControllerAdded &&
-        !isCollyAdded &&
-        !isEnemyCreatorAdded &&
-        !isCreatorLeftAdded &&
-        !isCreatorRightAdded) {
-      game.world.add(player = Earth(
-        position: Vector2(gameWidth / 2, gameHeight / 2),
-        paint: Paint()..color = Colors.red,
-      ));
-      game.world.add(controller = Radi(
-        position: Vector2(gameWidth / 2, gameHeight / 2),
-        paint: Paint()..color = Colors.blue,
-      ));
-      game.world.add(Colly(
-        position: Vector2(gameWidth / 2, gameHeight / 2),
-        paint: Paint()..color = Colors.yellow,
-      ));
-      game.world.add(EnemyCreator(
-        position: Vector2(gameWidth / 2, gameHeight / 2),
-        paint: Paint()..color = Colors.green,
-      ));
-      game.world.add(CreatorLeft(
-        position: Vector2(gameWidth / 2, gameHeight / 2),
-        paint: Paint()..color = Colors.green,
-      ));
-      game.world.add(CreatorRight(
-        position: Vector2(gameWidth / 2, gameHeight / 2),
-        paint: Paint()..color = Colors.green,
-      ));
-      removeFromParent();
+    Future.delayed(const Duration(seconds: 5), () {
+      if (!isPlayerAdded &&
+          !isControllerAdded &&
+          !isCollyAdded &&
+          !isEnemyCreatorAdded &&
+          !isCreatorLeftAdded &&
+          !isCreatorRightAdded) {
+        game.world.add(player = Earth(
+          position: Vector2(gameWidth / 2, gameHeight / 2),
+          paint: Paint()..color = Colors.red,
+        ));
+        game.world.add(controller = Radi(
+          position: Vector2(gameWidth / 2, gameHeight / 2),
+          paint: Paint()..color = Colors.blue,
+        ));
+        game.world.add(Colly(
+          position: Vector2(gameWidth / 2, gameHeight / 2),
+          paint: Paint()..color = Colors.yellow,
+        ));
+        game.world.add(EnemyCreator(
+          position: Vector2(gameWidth / 2, gameHeight / 2),
+          paint: Paint()..color = Colors.green,
+        ));
+        game.world.add(CreatorLeft(
+          position: Vector2(gameWidth / 2, gameHeight / 2),
+          paint: Paint()..color = Colors.green,
+        ));
+        game.world.add(CreatorRight(
+          position: Vector2(gameWidth / 2, gameHeight / 2),
+          paint: Paint()..color = Colors.green,
+        ));
+        removeFromParent();
 
-      // List<Vector2> positions = [
-      //   Vector2(gameWidth * 0.25, gameHeight * 0.25),
-      //   Vector2(gameWidth * 0.75, gameHeight * 0.25),
-      //   Vector2(gameWidth * 0.25, gameHeight * 0.75),
-      //   Vector2(gameWidth * 0.75, gameHeight * 0.75),
-      // ];
+        // List<Vector2> positions = [
+        //   Vector2(gameWidth * 0.25, gameHeight * 0.25),
+        //   Vector2(gameWidth * 0.75, gameHeight * 0.25),
+        //   Vector2(gameWidth * 0.25, gameHeight * 0.75),
+        //   Vector2(gameWidth * 0.75, gameHeight * 0.75),
+        // ];
 
-      // for (int i = 0; i < 4; i++) {
-      //   Test test = Test(
-      //     position: Vector2.zero(),
-      //     paint: Paint()..color = Colors.green,
-      //   );
-      //   test.x = positions[i].x;
-      //   test.y = positions[i].y;
-      //   world.add(test);
-      // }
+        // for (int i = 0; i < 4; i++) {
+        //   Test test = Test(
+        //     position: Vector2.zero(),
+        //     paint: Paint()..color = Colors.green,
+        //   );
+        //   test.x = positions[i].x;
+        //   test.y = positions[i].y;
+        //   world.add(test);
+        // }
 
-      isPlayerAdded = true;
-      isControllerAdded = true;
-      isCollyAdded = true;
-      isEnemyCreatorAdded = true;
-      isCreatorLeftAdded = true;
-      isCreatorRightAdded = true;
+        isPlayerAdded = true;
+        isControllerAdded = true;
+        isCollyAdded = true;
+        isEnemyCreatorAdded = true;
+        isCreatorLeftAdded = true;
+        isCreatorRightAdded = true;
 
-      // for (int i = 0; i < 10; i++) {
-      //   final randomX =
-      //       rand.nextDouble() * (gameWidth - plasticRadius * 2) + plasticRadius;
-      //   final randomY =
-      //       rand.nextDouble() * (gameHeight * 0.6 - plasticRadius * 2) +
-      //           plasticRadius;
+        // for (int i = 0; i < 10; i++) {
+        //   final randomX =
+        //       rand.nextDouble() * (gameWidth - plasticRadius * 2) + plasticRadius;
+        //   final randomY =
+        //       rand.nextDouble() * (gameHeight * 0.6 - plasticRadius * 2) +
+        //           plasticRadius;
 
-      //   world.add(Plastic(
-      //     difficultyModifier: difficultyModifier,
-      //     radius: plasticRadius,
-      //     position: Vector2(randomX, randomY),
-      //     velocity: Vector2((rand.nextDouble() - 0.5) * width, height * 0.2)
-      //         .normalized()
-      //       ..scale(height / 4),
-      //     earth: player,
-      //   ));
-      // }
-    }
+        //   world.add(Plastic(
+        //     difficultyModifier: difficultyModifier,
+        //     radius: plasticRadius,
+        //     position: Vector2(randomX, randomY),
+        //     velocity: Vector2((rand.nextDouble() - 0.5) * width, height * 0.2)
+        //         .normalized()
+        //       ..scale(height / 4),
+        //     earth: player,
+        //   ));
+        // }
+      }
+    });
   }
 }

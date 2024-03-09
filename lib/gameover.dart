7740176d8c80528d0ec8130cf75a726e76141e9e
Masda <matlabnewone@gmail.com>
2024-03-09 17:05:23 +0400

    Fix bug in login functionality

---
+++ lib/gameover.dart
@@ -0,0 +1,31 @@
+import 'dart:ui';
+
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+
+class GameOver extends RectangleComponent {
+  @override
+  GameOver({required Vector2 position, required paint})
+      : super(
+          anchor: Anchor.center,
+          size: Vector2(4000, 1080),
+          position: Vector2(1000, -600),
+
+          // ignore: prefer_const_constructors
+          paint: Paint()..color = Color.fromARGB(255, 137, 12, 3),
+          children: [
+            RectangleHitbox(size: Vector2(4000, 1080)),
+          ],
+        );
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+    size = Vector2(4000, 1080);
+    // add(SpriteComponent(
+    //   sprite: await Sprite.load('gameover.png'),
+    //   size: Vector2(640, 635),
+    //   position: Vector2(-10, -2),
+    //   // anchor: Anchor.topCenter, // BU ONEMLIDI  DO NOT ADD ANCHOR HERE
+    // ));
+  }
+}

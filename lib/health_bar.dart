2b9e57540c815a801fd0eaec20b93575950cb3cf
Masda <matlabnewone@gmail.com>
2024-03-09 09:54:44 +0400

    Refactor HealthBar class to adjust heart position

--- lib/health_bar.dart
+++ lib/health_bar.dart
@@ -2,6 +2,9 @@ import 'package:earth/game.dart';
 import 'package:flame/components.dart';
 
 class HealthBar extends PositionComponent with HasGameRef<MyGame> {
+  // HealthBar() {
+  //   priority = 10; // Higher values draw the component on top
+  // }
   int health = 5;
   List<SpriteComponent> hearts = [];
 
@@ -10,11 +13,11 @@ class HealthBar extends PositionComponent with HasGameRef<MyGame> {
     super.onLoad();
     for (var i = 0; i < health; i++) {
       var heart = SpriteComponent(
-        sprite: await Sprite.load('heart_a.png'),
-        size: Vector2(40, 35),
-        position: Vector2(i * 50.0 + 70,
-            gameRef.size.y - 1030), // Adjust position for each heart
-      );
+          sprite: await Sprite.load('heart_a.png'),
+          size: Vector2(40, 35),
+          position: Vector2(i * 50.0 + 70, gameRef.size.y - 1030),
+          priority: 2 // Adjust position for each heart
+          );
 
       hearts.add(heart);
       gameRef.world.add(heart);

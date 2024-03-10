37c3df73c14603c362efef1fbb1b985db40695ab
Masda <matlabnewone@gmail.com>
2024-03-10 19:24:04 +0400

    Fix bug in login functionality

---
+++ lib/components.dart
@@ -0,0 +1,10 @@
+export 'enemy/plastic.dart';
+export 'enemy/banana.dart';
+export 'earth.dart';
+export 'bin.dart';
+export 'buttons/start_button.dart';
+export 'other/test.dart';
+export 'play_area.dart';
+export 'enemy/creator_left.dart';
+export 'enemy/creator_right.dart';
+export 'enemy/enemy_creator.dart';

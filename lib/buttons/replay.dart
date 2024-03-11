673c6a4f029b408432bcb900e4140cb7ca7f69a1
Masda <matlabnewone@gmail.com>
2024-03-11 10:25:20 +0400

    Refactor Replay button and add fade-out effect

--- lib/buttons/replay.dart
+++ lib/buttons/replay.dart
@@ -1,29 +1,55 @@
+import 'dart:async';
+
 import 'package:earth/game.dart';
 
 import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
 import 'package:flame/events.dart';
 
 import 'package:flutter/widgets.dart';
 
 class Replay extends RectangleComponent
     with TapCallbacks, HasGameReference<MyGame> {
-  Replay()
-      : super(
-          size: Vector2(100, 80),
-          position: Vector2(800, 600),
-          paint: Paint()..color = const Color(0xFFFF0000),
-          priority: 5,
-        );
+  // ignore: prefer_const_constructors
+  Replay() : super(paint: Paint()..color = Color.fromARGB(0, 29, 144, 239)) {
+    width = 440;
+    height = 135;
+    x = 100;
+    y = 100;
+    position = Vector2(962, 700);
+    anchor = Anchor.center;
+  }
+  late final SpriteComponent sprite;
   @override
-  Future<void> onLoad() async {
-    super.onLoad();
-    size = Vector2(100, 80);
-    position = Vector2(920, 600);
+  FutureOr<void> onLoad() async {
+    sprite = SpriteComponent(
+        sprite: await Sprite.load('replay.png'),
+        position: Vector2(221, 70),
+        anchor: Anchor.center);
+    add(sprite);
+    sprite.add(
+      OpacityEffect.fadeOut(
+        EffectController(
+          duration: 0.7,
+          reverseDuration: 1.0,
+          infinite: true,
+        ),
+      ),
+    );
   }
 
   @override
   void onTapDown(TapDownEvent event) {
     game.resetGame();
+    add(
+      ScaleEffect.to(
+        Vector2.all(0.9),
+        SequenceEffectController([
+          LinearEffectController(0.1),
+          ReverseLinearEffectController(0.1),
+        ]),
+      ),
+    );
     // game.world.removeFromParent();
     // runApp(GameWidget(game: MyGame()));
   }
